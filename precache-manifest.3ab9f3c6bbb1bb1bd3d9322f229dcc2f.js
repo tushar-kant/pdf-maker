73508edb3ea4ed4b9ccf1405682093fe2b68e9df
tushar-kant <tusharkantanayak713@gmail.com>
2021-10-04 22:40:18 +0530

    Updates

---
+++ precache-manifest.3ab9f3c6bbb1bb1bd3d9322f229dcc2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce28f8f9a222b2d2c1f444d41b4d7f77",
+    "url": "/pdf-maker/index.html"
+  },
+  {
+    "revision": "28b5acc70735f08c77a5",
+    "url": "/pdf-maker/static/css/main.6bfee1dc.chunk.css"
+  },
+  {
+    "revision": "0e548820ed2a20916811",
+    "url": "/pdf-maker/static/js/2.dd1b76e8.chunk.js"
+  },
+  {
+    "revision": "c18d21cfab8ed33406204ad5badd10a3",
+    "url": "/pdf-maker/static/js/2.dd1b76e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b5acc70735f08c77a5",
+    "url": "/pdf-maker/static/js/main.62648d1a.chunk.js"
+  },
+  {
+    "revision": "18658e4bba6dae2fff1a",
+    "url": "/pdf-maker/static/js/runtime-main.6a2c8bbc.js"
+  }
+]);
