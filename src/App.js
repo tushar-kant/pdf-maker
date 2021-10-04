5c5bcd8341496c55bc95e664378987e41f89762f
tushar-kant <tusharkantanayak713@gmail.com>
2021-10-04 22:20:43 +0530

    'first'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Post from './components/Post';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Post />
+    </div>
+  );
+}
+
+export default App;
